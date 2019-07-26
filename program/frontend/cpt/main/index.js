61ba84810e7e2ac87fadc956e041d766bea6c446
lhm <laihuanminpc>
2019-07-26 22:44:14 +0800

    add react service, but will change to vue

--- program/frontend/cpt/main/index.js
+++ program/frontend/cpt/main/index.js
@@ -1,6 +1,12 @@
+import { Layout } from 'antd';
+import { Tabs } from 'antd';
+const { TabPane } = Tabs;
+
 @gcpt
 export default class AppPage {
+	handleTabChange() {}
+
 	render() {
-		return <div>main</div>;
+		return <div />;
 	}
 }
